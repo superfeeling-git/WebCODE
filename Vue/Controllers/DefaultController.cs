1c587d5308c752476329058cf96f6f1dab2316b4
superfeeling-git <58456025+superfeeling-git@users.noreply.github.com>
2020-02-08 00:45:31 +0800

    添加项目文件。

---
+++ Vue/Controllers/DefaultController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vue.Models;
+using System.Net.Http;
+
+namespace Vue.Controllers
+{
+    public class DefaultController : Controller
+    {
+        dechengEntities db = new dechengEntities();
+
+        // GET: Default
+        public ActionResult Index(int? pageIndex)
+        {
+            if(Request.IsAjaxRequest())
+            {
+                int page = pageIndex ?? 1;
+                int PageSize = 12;
+                int skip = ((int)page - 1) * PageSize;
+
+                return Json(db.dc_Log.OrderBy(m => m.AddTime).Skip(skip).Take(PageSize).ToList(), JsonRequestBehavior.AllowGet);
+            }
+            return View();
+        }
+
+        public ActionResult Api()
+        {
+            return View();
+        }
+    }
+}
